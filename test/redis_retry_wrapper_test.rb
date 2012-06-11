998f035a94fcf55f96d37aa19dbd52db7c277935
Ryan Schlesinger <ryan@instanceinc.com>
2012-06-11 16:11:10 -0700

    Converting tests from backported patches

--- test/redis_retry_wrapper_test.rb
+++ test/redis_retry_wrapper_test.rb
@@ -1,19 +1,19 @@
 require 'test_helper'
 
-describe "Resque::RedisRetryWrapper" do
+context "Resque::RedisRetryWrapper" do
   include Test::Unit::Assertions
   
-  before do
+  setup do
     @redis_wrapper = Resque::RedisRetryWrapper.new(Resque.redis)
     Resque::RedisRetryWrapper.any_instance.stubs(:sleep_seconds).returns(0) # retry faster to speed up the tests
   end
   
-  it 'sends commands to Redis' do
+  test 'sends commands to Redis' do
     Redis.any_instance.expects(:hello).with(nil)
     @redis_wrapper.hello
   end
 
-  it 'retries commands upon receiving connection errors' do
+  test 'retries commands upon receiving connection errors' do
     Redis::Client.any_instance.expects(:connected?).at_least(3).returns(false)
     Redis::Client.any_instance.expects(:establish_connection).at_least(3).raises(Errno::EAGAIN)
     
@@ -22,7 +22,7 @@ describe "Resque::RedisRetryWrapper" do
   
   # this is our block test. a stubbed block test doesn't appear to be possible in mocha right now
   # http://stackoverflow.com/questions/3252046/mock-methods-that-receives-a-block-as-parameter
-  it 'runs multi' do
+  test 'runs multi' do
     @redis_wrapper.multi do
       Resque.redis.set "foo", "bar"
       Resque.redis.incr "baz"
